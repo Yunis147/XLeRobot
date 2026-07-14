ce782c7c755169089d4788c0d4d9c18c9a04233d
yunis <shaikyunis181@gmail.com>
2026-07-14 10:45:36 +0530

    added files for nav2 and modified params

--- nav2/nav2/launch/base.nav2.py
+++ nav2/nav2/launch/base.nav2.py
@@ -1,81 +1,41 @@
 import os
-
 from launch import LaunchDescription
+from launch_ros.actions import Node
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.actions import Node
-
 from ament_index_python.packages import get_package_share_directory
-
 
 def generate_launch_description():
 
-    # Package directories
     nav2_bringup_dir = get_package_share_directory('nav2_bringup')
+    nav2_launch_file = os.path.join(nav2_bringup_dir, 'launch', 'bringup_launch.py')
+
     rplidar_dir = get_package_share_directory('rplidar_ros')
+    rplidar_launch_file = os.path.join(rplidar_dir, 'launch', 'rplidar_a1_launch.py')
 
-    # Launch files
-    nav2_launch = os.path.join(
-        nav2_bringup_dir,
-        'launch',
-        'bringup_launch.py'
-    )
-
-    rplidar_launch = os.path.join(
-        rplidar_dir,
-        'launch',
-        'rplidar_a1_launch.py'
-    )
-
-    # Your saved map
     map_file = '/home/rpd/lekiwi_ws/src/mobile-manipulator/nav2/nav2/map/base_only_map.yaml'
-
-    # Nav2 parameters
-    params_file = '/home/rpd/lekiwi_ws/src/mobile-manipulator/nav2/nav2/params/dwb_params.yaml'
+    params_file = '/home/rpd/lekiwi_ws/src/mobile-manipulator/nav2/nav2/params/my_nav2_params.yaml'
 
     return LaunchDescription([
 
-        #
-        # Start RPLidar
-        #
         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(rplidar_launch),
-            launch_arguments={
-                'serial_port': '/dev/ttyUSB0'
-            }.items()
+            PythonLaunchDescriptionSource(rplidar_launch_file),
+            launch_arguments={'serial_port': '/dev/ttyUSB0'}.items()
         ),
 
-        #
-        # base_link -> laser
-        #
         Node(
             package='tf2_ros',
             executable='static_transform_publisher',
             name='base_to_laser_tf',
-            arguments=[
-                '0.01',
-                '0.0',
-                '0.3',
-                '-1.57',
-                '0.0',
-                '0.0',
-                'base_link',
-                'laser'
-            ]
+            arguments=['0.01', '0.0', '0.3', '-1.57', '0.0', '0.0', 'base_link', 'laser']
         ),
 
-        #
-        # Nav2
-        #
         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(nav2_launch),
+            PythonLaunchDescriptionSource(nav2_launch_file),
             launch_arguments={
-                'slam': 'False',
-                'use_localization': 'True',
                 'map': map_file,
-                'params_file': params_file,
                 'use_sim_time': 'false',
-                'autostart': 'true'
+                'params_file': params_file
             }.items()
         ),
     ])
