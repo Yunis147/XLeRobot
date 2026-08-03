58b2f4b561b5d7658697945b5d360c17d25a6253
yunis <shaikyunis181@gmail.com>
2026-08-03 16:44:46 +0530

    changed some files

--- nav2/nav2/launch/xle.nav2.py
+++ nav2/nav2/launch/xle.nav2.py
@@ -11,6 +11,7 @@ def generate_launch_description():
     filter_yaml = '/home/rpd/lekiwi_ws/src/mobile-manipulator/nav2/nav2/params/laser_filter.yaml'
     nav2_params = '/home/rpd/lekiwi_ws/src/mobile-manipulator/nav2/nav2/params/dwb_params.yaml'
 
+    # RPLidar Node
     rplidar_node = Node(
         package='rplidar_ros',
         executable='rplidar_node',
@@ -25,6 +26,7 @@ def generate_launch_description():
         remappings=[('scan', '/scan_raw')]
     )
 
+    # Laser Filter Node
     laser_filter_node = Node(
         package='laser_filters',
         executable='scan_to_scan_filter_chain',
@@ -37,13 +39,27 @@ def generate_launch_description():
         ]
     )
 
-    static_tf = Node(
+    # Corrected Base-to-Laser TF (-1.57 Yaw restore)
+    static_tf_laser = Node(
         package='tf2_ros',
         executable='static_transform_publisher',
         name='base_to_laser_tf',
-        arguments=['0.01', '0.0', '0.6', '1.57', '0.0', '0.0', 'base_link', 'laser']
+        arguments=['--x', '0.01', '--y', '0.0', '--z', '0.6', 
+                   '--yaw', '-1.57', '--pitch', '0.0', '--roll', '0.0', 
+                   '--frame-id', 'base_link', '--child-frame-id', 'laser']
     )
 
+    # Restored Footprint-to-Link TF
+    static_tf_footprint = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='footprint_to_link_tf',
+        arguments=['--x', '0.0', '--y', '0.0', '--z', '0.0', 
+                   '--yaw', '0.0', '--pitch', '0.0', '--roll', '0.0', 
+                   '--frame-id', 'base_footprint', '--child-frame-id', 'base_link']
+    )
+
+    # Nav2 Bringup
     nav2_bringup_dir = get_package_share_directory('nav2_bringup')
     nav2_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -59,6 +75,7 @@ def generate_launch_description():
     return LaunchDescription([
         rplidar_node,
         laser_filter_node,
-        static_tf,
+        static_tf_laser,
+        static_tf_footprint,
         nav2_launch
     ])
